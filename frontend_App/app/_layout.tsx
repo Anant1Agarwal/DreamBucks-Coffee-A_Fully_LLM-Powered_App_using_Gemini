45761a20921c303fe34a3f084547a049ff8b0366
Anant1Agarwal <anant16agarwal@gmail.com>
2024-12-31 10:55:36 +0530

    done with Start page

--- frontend_App/app/_layout.tsx
+++ frontend_App/app/_layout.tsx
@@ -1,15 +1,23 @@
-
-import { Stack } from 'expo-router/stack';
+import { Stack } from "expo-router/stack";
 import "../global.css";
 import { useFonts } from "expo-font";
 // import { NativeWindStyleSheet } from "nativewind";
 // import { styled } from 'nativewind';
 
-
-
-
 export default function RootLayout() {
-  return <Stack >
-    <Stack.Screen name="index"/>
-  </Stack>
+  const [fontsLoaded] = useFonts({
+    "Sora-Regular": require("../assets/fonts/Sora-Regular.ttf"),
+    "Sora-SemiBold": require("../assets/fonts/Sora-SemiBold.ttf"),
+    "Sora-Bold": require("../assets/fonts/Sora-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return undefined;
+  }
+  
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- frontend_App/app/index.tsx
+++ frontend_App/app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView, ImageBackground } from "react-native";
+import {
+  GestureHandlerRootView,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-3xl text-orange-500">Hey from Coffee shop. This is Anant Agarwal. I am learning App Development</Text>
-    </View>
+    <GestureHandlerRootView>
+      <View className="w-full h-full">
+        <ImageBackground
+          className="w-full h-full items-center"
+          source={require("../assets/images/index_bg_image.png")}
+        >
+          <View className="flex h-[48%]" />
+          <View className="flex w-[80%]">
+            <Text className="text-white text-[40px] font-bold text-left font-[Sora-Bold] ">
+              Welcome to DreamBucks Coffee!
+            </Text>
+
+            <Text className="pt-3 mt-2 mb-28 text-white text-[21px] text-left font-[Sora-Regular]">
+              Come Fall in Love with coffee.
+            </Text>
+    
+              <TouchableOpacity
+                style={{
+                  marginTop: 18 ,
+                  backgroundColor: "#1E3932",
+                  padding: 12,
+                  borderRadius: 8,
+                  alignItems: "center",
+                }}
+              >
+                <Text className="text-xl text-white font-[Sora-SemiBold]">
+                  Get Started
+                </Text>
+              </TouchableOpacity>
+    
+           
+          </View>
+        </ImageBackground>
+      </View>
+    </GestureHandlerRootView>
   );
 }
