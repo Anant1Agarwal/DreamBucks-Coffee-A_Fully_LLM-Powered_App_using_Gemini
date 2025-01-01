2ceaf1e9b90fe0f8684b795e0e75ac412ad2ea22
Anant1Agarwal <anant16agarwal@gmail.com>
2025-01-01 18:26:17 +0530

    cart product list done

--- frontend_App/app/(tabs)/orderDetails.tsx
+++ frontend_App/app/(tabs)/orderDetails.tsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import PageHeader from '@/components/PageHeader'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import PageHeader from "@/components/PageHeader";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ProductList from "@/components/CartProductList";
+import { Product } from "@/Object_Types/types";
+import { useCart } from "@/components/CartContext";
+import { fetchProducts } from "@/services/Product_Services";
 const orderDetails = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const { cartItems, setQuantityinCart, emptyCart } = useCart();
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const productData = await fetchProducts();
+        setProducts(productData);
+      } catch (e) {
+        console.log("Error in loading products at cart page");
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadProducts();
+  }, []);
+
   return (
-    <View>
-      <PageHeader title="Order Details" showHeaderRight={false} bgColor='#F9F9F9'/>
-      <Text>orderDetails</Text>
-    </View>
-  )
-}
+    <GestureHandlerRootView className="h-full w-full bg-[#F9F9F9]">
+      <PageHeader
+        title="Order Details"
+        showHeaderRight={false}
+        bgColor="#F9F9F9"
+      />
+      <View className="h-full flex-col justify-between">
+        <View className="h-[75%] ">
+          <ProductList products={products} quantities={cartItems} setQuantities={setQuantityinCart} totalPrice={totalPrice} />
+        </View>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
 
-export default orderDetails
-
+export default orderDetails;
